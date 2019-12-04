a7638b105b540a1a2f00048e84ba01b8d16611c0
peng_xiaochun <peng_xiaochun@hoperun.com>
2019-12-04 15:20:20 +0800

    高可用分布式配置中心

--- config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+//@EnableDiscoveryClient
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
