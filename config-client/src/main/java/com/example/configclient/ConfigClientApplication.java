551f038ad6bd20f7c6172d09ff888069b3b541b8
peng_xiaochun <peng_xiaochun@hoperun.com>
2019-12-10 09:23:06 +0800

    消息总线

--- config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -3,12 +3,14 @@ package com.example.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 //@EnableDiscoveryClient
+@RefreshScope
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
